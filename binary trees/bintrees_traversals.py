39e6b22bb16c6f407b0468ebd4cdb74f1b1495bf
Nathalie <nathalie.bouquet@epita.fr>
2019-10-21 19:49:08 +0200

    Update and rename bintrees_traversals_FIXME.py to bintrees_traversals.py

--- binary trees/bintrees_traversals.py
+++ binary trees/bintrees_traversals.py
@@ -62,11 +62,52 @@ def bfs(B):
             if B.right != None:
                 q.enqueue(B.right)
 
-
+# with level change marks (None)
 def width(B):
     """
     Computes B width (largeur)
     """
-    #FIXME
-    pass
-    
+    w_max = 0
+    if B:
+        q = queue.Queue()
+        q.enqueue(B)
+        q.enqueue(None)
+        w = 0
+        while not q.isempty():
+            B = q.dequeue()
+            if B == None:
+                w_max = max(w, w_max)
+                if not q.isempty():
+                    q.enqueue(None)
+                    w = 0
+            else:
+                w = w + 1
+                if B.left:
+                    q.enqueue(B.left)
+                if B.right:
+                    q.enqueue(B.right)
+    return w
+
+# another way to manage levels, with two queues.                    
+def width2(B): 
+    """
+    computes the width of a bintree
+    """
+    w_max = 0
+    if B != None:
+        q = queue.Queue() #current
+        q.enqueue(B)
+        q_next = queue.Queue() #next level
+        w = 0
+        while not q.isempty():
+            B = q.dequeue()
+            w = w + 1
+            if B.left != None:
+                q_next.enqueue(B.left)
+            if B.right != None:
+                q_next.enqueue(B.right)
+            if q.isempty():
+                w_max = max(w, w_max)
+                (q, q_next) = (q_next, q)
+                w = 0
+    return w_max
